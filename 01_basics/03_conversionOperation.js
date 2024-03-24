ff3aaea5ae09dede8b8c111a213d641f09179fd4
shreyash2410 <125814983+shreyash2410@users.noreply.github.com>
2024-03-24 12:02:12 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* Score = 33 =>O/P 33
+/ Score = 33abc =>O/P NaN (Not a Number)
+/ Score = null =>O/P 0
+/ Score = undefined => NaN
+/ Score = true =>O/P 1
+  Score = "Shreyash" =>O/P NaN
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/*
+    1 => true; 0 => false
+    "" => false
+    "Shreyash" => true
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
